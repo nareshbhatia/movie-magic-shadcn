e5202e387ae541eeff82b66816a6fb1ed0ee6a82
Naresh Bhatia <me@nareshbhatia.dev>
2024-02-11 15:29:17 -0500

    feat: changed ModeToggle to support system, in addition to light & dark

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,13 @@
-import { Check, ChevronDown, Heart, Moon, SunMedium } from 'lucide-react';
+import {
+  Check,
+  ChevronDown,
+  ChevronRight,
+  ChevronUp,
+  Circle,
+  Heart,
+  Moon,
+  SunMedium,
+} from 'lucide-react';
 import type { LucideIcon } from 'lucide-react';
 
 export type Icon = LucideIcon;
@@ -6,6 +15,9 @@ export type Icon = LucideIcon;
 export const Icons = {
   check: Check,
   chevronDown: ChevronDown,
+  chevronRight: ChevronRight,
+  chevronUp: ChevronUp,
+  circle: Circle,
   heart: Heart,
   moon: Moon,
   sun: SunMedium,
